f48a822d6de1a72a965d2b67755325e0fe049c87
SamHuang <sam821203@yahoo.com.tw>
2023-04-15 12:40:52 +0800

    add todo-list

---
+++ to-do-list/app.js
@@ -0,0 +1,47 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: ''
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = '';
+    }
+  }
+}).mount('#app');
+
+// Vue.createApp({
+//   data() {
+//     return {
+//       goals: [],
+//       enteredValue: ''
+//     };
+//   },
+//   methods: {
+//     addGoal() {
+//       this.goals.push(this.enteredValue);
+//       this.enteredValue = '';
+//     }
+//   }
+// }).mount('#app');
+
+// const inputEl = document.querySelector('input');
+// const buttonEl = document.querySelector('button');
+// const listEl = document.querySelector('ul');
+
+// function addGoal() {
+//   const enteredValue = inputEl.value;
+//   const listItemEl = document.createElement('li');
+//   listItemEl.textContent = enteredValue;
+
+//   listEl.appendChild(listItemEl);
+
+//   inputEl.value = '';
+// }
+
+// buttonEl.addEventListener('click', addGoal)
