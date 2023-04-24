afd935bcea6a5a6cb25ac7fbcc1e93e84ffa6075
SamHuang <sam821203@yahoo.com.tw>
2023-04-24 23:00:42 +0800

    complete Maximilian Vue chapter 4-53

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -9,10 +9,23 @@ createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      currentRound: 0,
     };
+  },
+  computed: {
+    monsterBarStyle() {
+      return { width: this.monsterHealth + '%' }
+    },
+    playerBarStyle() {
+      return { width: this.playerHealth + '%' }
+    },
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    }
   },
   methods: {
     attackMonster() {
+      this.currentRound++;
       const attackPoint = getRandomValue(5, 20);
       this.monsterHealth -= attackPoint;
       this.attackPlayer();
@@ -20,6 +33,12 @@ createApp({
     attackPlayer() {
       const attackPoint = getRandomValue(10, 25);
       this.playerHealth -= attackPoint;
+    },
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackPoint = getRandomValue(15, 30);
+      this.monsterHealth -= attackPoint;
+      this.attackPlayer();
     }
   }
 }).mount('#game');
