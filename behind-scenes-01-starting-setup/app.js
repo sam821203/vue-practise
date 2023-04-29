21d15c14565837d000a0ac13c1229577af354e31
SamHuang <sam821203@yahoo.com.tw>
2023-04-29 11:07:43 +0800

    complete Maximilian Vue chapter 5

---
+++ behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,80 @@
+const { createApp } = Vue;
+
+const app = createApp({
+  data() {
+    return {
+      currentUserInput: '',
+      message: 'Vue is great!',
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      // this.message = this.currentUserInput;
+      this.message = this.$refs.userText.value;
+    },
+  },
+  beforeCreate() {
+    console.log('beforeCreate()');
+  },
+  created() {
+    console.log('created()');
+  },
+  beforeMount() {
+    console.log('beforeMount()');
+  },
+  mounted() {
+    console.log('mounted()');
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate()');
+  },
+  updated() {
+    console.log('updated()');
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount()');
+  },
+  unmounted() {
+    console.log('unmounted()');
+  }
+});
+
+app.mount('#app');
+
+setTimeout(function () {
+  app.unmount();
+}, 3000);
+
+createApp({
+  template: `
+    <p>{{ favoriteMeal }}</p>
+  `,
+  data() {
+    return {
+      favoriteMeal: 'Pizza',
+    }
+  }
+}).mount('#app2')
+
+const data = {
+  message: 'Hello!',
+  longMessage: 'Hello! World!',
+};
+
+const handler = {
+  set(target, key, value) {
+    if (key === 'message') {
+      target.longMessage = value + ' World!';
+    }
+    target.message = value;
+  }
+}
+
+const proxy = new Proxy(data, handler);
+
+proxy.message = 'Hello!!!!!';
+
+// console.log(proxy.longMessage);
