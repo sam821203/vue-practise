36c45516f2b6c3a56105485275d3d4b331123659
SamHuang <sam821203@yahoo.com.tw>
2023-05-15 23:53:22 +0800

    complete Maximilian Vue chapter 14

--- animations-01-starting-setup/src/App.vue
+++ animations-01-starting-setup/src/App.vue
@@ -1,18 +1,27 @@
 <template>
+  <router-view v-slot="slotProps">
+    <transition name="fade-button" mode="out-in">
+      <component :is="slotProps.Component"></component>
+    </transition>
+  </router-view>
+  <!-- <div class="container">
+    <user-list></user-list>
+  </div>
   <div class="container">
     <div class="block" :class="{ animate: animatedBlock }"></div>
     <button @click="animateBlock">Animate</button>
   </div>
   <div class="container">
     <transition
-      enter-active-class="para-moving-x"
-      leave-active-class="para-moving-x"
+      :css="false"
       @before-enter="beforeEnter"
       @before-leave="beforeLeave"
       @enter="enter"
       @after-enter="afterEnter"
       @leave="leave"
       @after-leave="afterLeave"
+      @enter-cancelled="enterCancelled"
+      @leave-cancelled="leaveCancelled"
     >
       <p v-if="paraIsVisible">New Paragraph</p>
     </transition>
@@ -30,39 +39,81 @@
   </base-modal>
   <div class="container">
     <button @click="showDialog">Show Dialog</button>
-  </div>
+  </div> -->
 </template>
 
 <script>
+// import UserList from './components/UserList.vue';
+
 export default {
+  // components: {
+  //   UserList,
+  // },
   data() {
     return {
       animatedBlock: false,
       dialogIsVisible: false,
       paraIsVisible: false,
       usersAreVisible: false,
+      enterInterval: null,
+      leaveInterval: null,
     };
   },
   methods: {
+    enterCancelled(el) {
+      console.log('enterCancelled');
+      console.log(el);
+      clearInterval(this.enterInterval);
+    },
+    leaveCancelled(el) {
+      console.log('leaveCancelled');
+      console.log(el);
+      clearInterval(this.leaveInterval);
+    },
     beforeEnter(el) {
       console.log('beforeEnter');
       console.log(el);
+      el.style.opacity = 0;
     },
-    beforeLeave(el) {
-      console.log('beforeLeave');
-      console.log(el);
-    },
-    enter(el) {
+    enter(el, done) {
       console.log('enter');
       console.log(el);
+      let round = 1;
+      this.enterInterval = setInterval(() => {
+        el.style.opacity = round * 0.01;
+
+        round++;
+
+        if (round > 100) {
+          clearInterval(this.enterInterval);
+          // 告訴 vue 何時完成
+          done();
+        }
+      }, 20);
     },
     afterEnter(el) {
       console.log('afterEnter');
       console.log(el);
     },
-    leave(el) {
+    beforeLeave(el) {
+      console.log('beforeLeave');
+      console.log(el);
+      el.style.opacity = 1;
+    },
+    leave(el, done) {
       console.log('leave');
       console.log(el);
+      let round = 1;
+      this.leaveInterval = setInterval(() => {
+        el.style.opacity = 1 - round * 0.01;
+
+        round++;
+
+        if (round > 100) {
+          clearInterval(this.leaveInterval);
+          done();
+        }
+      }, 20);
     },
     afterLeave(el) {
       console.log('afterLeave');
@@ -158,19 +209,18 @@ button:active {
   transform: translateY(0);
 } */
 
-.para-leave-active {
-  /* transition: all 0.4s ease-in; */
+/* .para-leave-active {
   animation: moveX 0.3s ease-out;
-}
+} */
 
 /* .v-leave-to {
   opacity: 0;
   transform: translateY(-30px);
 } */
 
-.para-moving-x {
+/* .para-moving-x {
   animation: moveX 1s ease-out;
-}
+} */
 
 .fade-button-enter-from,
 .fade-button-leave-to {
@@ -185,6 +235,14 @@ button:active {
 .fade-button-enter-to,
 .fade-button-leave-from {
   opacity: 1;
+}
+
+.route-enter-active {
+  animation: moveX 0.3s ease-out;
+}
+
+.route-leave-active {
+  animation: moveX 0.3s ease-out;
 }
 
 @keyframes moveX {
