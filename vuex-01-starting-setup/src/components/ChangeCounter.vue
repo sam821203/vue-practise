79fabef1c99f4a2d83996ff7ac6336dddca9fd80
SamHuang <sam821203@yahoo.com.tw>
2023-05-17 23:40:47 +0800

    complete Maximilian Vue chapter 15-221

---
+++ vuex-01-starting-setup/src/components/ChangeCounter.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="inc">Add 2</button>
+  <button @click="increase({ value: 20 })">Add 20</button>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+export default {
+  methods: {
+    // addOne() {
+    //   this.$store.dispatch('increment');
+    // },
+    ...mapActions({
+      inc: 'increment',
+      increase: 'increase',
+    }),
+  },
+};
+</script>
