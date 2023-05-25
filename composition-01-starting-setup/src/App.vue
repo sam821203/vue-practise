606a69336a897d7f0b24bc5e946127023e5a8aa5
sam <sam821203@yahoo.com.tw>
2023-05-25 18:33:37 +0800

    complete Maximilian Vue chapter 19-289

---
+++ composition-01-starting-setup/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <section class="container">
+    <h2>{{ user.name }}</h2>
+    <h3>{{ user.age }}</h3>
+    <button @click="setNewData">Change Age</button>
+  </section>
+</template>
+
+<script setup>
+import { reactive } from 'vue';
+
+// const uName = ref('Sam')
+// const uAge = ref(10)
+
+const user = reactive({
+  name: 'Sam',
+  age: 20
+})
+
+const setNewData = () => {
+  user.age = 102;
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+.container {
+  margin: 3rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  text-align: center;
+}
+</style>
