1ceb4a0f34f5c3a42197c09188ad9edd7ac61c36
sam <sam821203@yahoo.com.tw>
2023-05-26 10:50:15 +0800

    complete Maximilian Vue chapter 19-assignment-7

--- composition-assignment-1-problem/src/App.vue
+++ composition-assignment-1-problem/src/App.vue
@@ -1,25 +1,56 @@
 <template>
-  <h2>My Course Goal</h2>
-  <!-- Task 1: Output your main course goal with help of the composition API -->
-  <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
-  <h3 v-if="showHide">{{ userGoal }}</h3>
-  <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
-  <button @click="toggleGoal">Toggle Goal</button>
-  <!-- Task 3: Manage data in three ways -->
-  <!-- => Separate refs -->
-  <!-- => Ref Object -->
-  <!-- => Reactive Object -->
-  <!-- Task 4: Also solve the assignment with the Options API -->
+  <div>
+    <h2>My Course Goal</h2>
+    <!-- Task 1: Output your main course goal with help of the composition API -->
+    <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
+    <h3 v-if="goalIsVisible">{{ goal }}</h3>
+    <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
+    <button @click="toggleGoal">Toggle Goal</button>
+    <!-- Task 3: Manage data in three ways -->
+    
+    <!-- Task 4: Also solve the assignment with the Options API -->
+  </div>
 </template>
 
-<script setup>
-import { ref } from 'vue';
+<!-- composition API ========================= -->
+<!-- <script setup>
+import { reactive } from 'vue';
+// => Separate refs
+// const courseGoal = ref('Across the stress')
+// let goalIsVisible = ref(false)
 
-const userGoal = ref('Across the stress')
-let showHide = ref(true)
+// => Ref Object
+// const courseData = ref({
+//   goal: 'Across the stress',
+//   goalIsVisible: false,
+// })
+
+// => Reactive Object
+const courseData = reactive({
+  goal: 'Across the stress',
+  goalIsVisible: false,
+})
 
 const toggleGoal = () => {
-  showHide.value = !showHide.value;
+  // goalIsVisible.value = !goalIsVisible.value;
+  courseData.goalIsVisible = !courseData.goalIsVisible
+}
+</script> -->
+
+<!-- Options API ========================= -->
+<script>
+export default {
+  data() {
+    return {
+      goal: 'Across the stress',
+      goalIsVisible: false,
+    }
+  },
+  methods: {
+    toggleGoal() {
+      this.goalIsVisible = !this.goalIsVisible
+    }
+  }
 }
 </script>
 
