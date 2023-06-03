ae9b39e2a02c2a4bea2be7621683d520aa2eca41
SamHuang <sam821203@yahoo.com.tw>
2023-06-03 14:30:24 +0800

    docs: add mike advanced document

---
+++ Mike (advanced)/CH7/exercise/Nuxt-Features/pages/index.vue
@@ -0,0 +1,25 @@
+<script>
+import login from "../components/login.vue";
+import TestBtn from "../components/TestBtn.vue";
+export default {
+  components: {
+    login,
+    TestBtn
+  }
+};
+</script>
+
+<template>
+  <div id="app">
+    <login />
+    <!-- <TestBtn/> -->
+  </div>
+</template>
+
+<style lang="scss" scoped>
+#app {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+</style>
