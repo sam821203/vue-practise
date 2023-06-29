4d4cdcfd386adb1f47edc7c06e19c265f946aa1d
SamHuang <sam821203@yahoo.com.tw>
2023-06-29 23:56:09 +0800

    complete: Andrei Neagoie section 5

--- Andrei Neagoie/section-4-1-STARTER/app.js
+++ Andrei Neagoie/section-4-1-STARTER/app.js
@@ -1,37 +1,29 @@
-let vm = Vue.createApp({
+let app = Vue.createApp({
+
+})
+
+app.component('hello', {
+  template: `<h1>{{ message }}</h1>`,
   data() {
     return {
-      message: "Hello world!"
+      message: 'Hello world!'
     }
-  },
-  beforeCreate() {
-    console.log('beforeCreate', this.message);
-  },
-  created() {
-    console.log('created', this.message);
-  },
-  beforeMount() {
-    console.log('beforeMount', this.$el);
-  },
-  mounted() {
-    console.log('mounted', this.$el);
-  },
-  beforeUpdate() {
-    console.log('beforeUpdate');
-  },
-  updated() {
-    console.log('updated');
-  },
-  beforeUnmount() {
-    console.log('beforeUnmount');
-  },
-  unmount() {
-    console.log('unmount');
   }
 })
 
-vm.mount('#app');
+app.mount('#app');
 
-// setTimeout(() => {
-//   vm.mount('#app');
-// }, 3000)
+// let vm2 = Vue.createApp({
+//   data() {
+//     return {
+//       message: "Hello world!"
+//     }
+//   },
+//   render() {
+//     return Vue.h(
+//       'h1',
+//       this.message
+//     )
+//   }
+// })
+// vm2.mount('#app2')
