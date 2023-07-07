140283f4d67367abbefbf5bd1d589a4ade01648a
SamHuang <sam821203@yahoo.com.tw>
2023-07-07 23:23:41 +0800

    complete: Andrei Neagoie section 6

---
+++ Andrei Neagoie/slots/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <app-form>
+      <template v-slot:help>
+        <p>{{ help }}</p>
+      </template>
+      <template v-slot:fields>
+        <input type="text" placeholder="email">
+        <input type="text" placeholder="username">
+        <input type="password" placeholder="password">
+      </template>
+      <template v-slot:buttons>Submit</template>
+      <p>Dummy text</p>
+    </app-form>
+    <app-form>
+      <template v-slot:help>
+        <p>Contact</p>
+      </template>
+      <template v-slot:fields>
+        <input type="text" placeholder="email">
+        <input type="text" placeholder="username">
+      </template>
+      <template v-slot:buttons>Submit</template>
+      <p>Dummy text</p>
+    </app-form>
+  </div>
+</template>
+
+<script >
+import AppForm from "./components/AppForm.vue"
+
+export default {
+  name: "App",
+  components: {
+    AppForm,
+  },
+  data() {
+    return {
+      help: 'This is some help text!'
+    }
+  }
+}
+</script>
---
+++ Andrei Neagoie/slots/src/components/AppForm.vue
@@ -0,0 +1,14 @@
+<template>
+  <form>
+    <div class="help">
+      <slot name="help"></slot>
+    </div>
+    <div class="fields">
+      <slot name="fields"></slot>
+    </div>
+    <button class="buttons">
+      <slot name="buttons"></slot>
+    </button>
+    <slot></slot>
+  </form>
+</template>
