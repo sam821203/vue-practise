22bb2a31161e73b36d50f186dcfdf8e078ac661c
SamHuang <sam821203@yahoo.com.tw>
2023-07-10 21:39:12 +0800

    complete: Andrei Neagoie section 8-82

---
+++ Andrei Neagoie/dynamic-component/src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Home</h1>
+</template>
+
+<script>
+export default {
+  unmounted() {
+    console.log('Home unmounted');
+  },
+  activated() {
+    console.log('Home activated');
+  },
+  deactivated() {
+    console.log('Home deactivated');
+  }
+}
+</script>
