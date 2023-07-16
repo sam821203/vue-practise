b26f36f356338981a01f9d86179d89825fbac055
SamHuang <sam821203@yahoo.com.tw>
2023-07-16 21:33:40 +0800

    complete: Andrei Neagoie section 13

--- Andrei Neagoie/music/src/includes/firebase.js
+++ Andrei Neagoie/music/src/includes/firebase.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
+import 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyA5YhTtZoaYmXsojwm7NRIOG95q9jBsoMI",
@@ -14,11 +15,15 @@ firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const db = firebase.firestore();
+const storage = firebase.storage();
 
 const userCollection = db.collection('users');
+const songsCollection = db.collection('songs');
 
 export {
   auth,
   db,
-  userCollection
+  userCollection,
+  songsCollection,
+  storage
 }
---
+++ Andrei Neagoie/section-10-12-STARTER/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <hello-world #default="{ user, favorites }">
+      <p>hello {{ user.name }}</p>
+      <p>My favorite food is {{ favorites[0] }}</p>
+    </hello-world>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
